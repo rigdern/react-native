3244de49f191307c02a1d2fe9475154ff814fe3a
Adam Comella <adcom@microsoft.com>
2015-11-16 17:27:59 -0800

    Make require extraction compatible with Windows line endings

--- packager/react-packager/src/DependencyResolver/Module.js
+++ packager/react-packager/src/DependencyResolver/Module.js
@@ -152,6 +152,7 @@ function extractRequires(code /*: string*/) /*: Array<string>*/ {
   };
 
   code
+    .replace('\r', '')
     .replace(blockCommentRe, '')
     .replace(lineCommentRe, '')
     // Parse sync dependencies. See comment below for further detils.
