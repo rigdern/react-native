ffeb920871b1b3b2bcd6cb6fea32b656807b6cb5
Adam Comella <adcom@microsoft.com>
2016-04-29 16:41:24 -0700

    Android: Support HTTP headers for source prop on <Image> components
    Allows developers to specify headers to include in the HTTP request
    when fetching a remote image. For example, one might leverage this
    when fetching an image from an endpoint that requires authentication:
    
    ```
    <Image
      style={styles.logo}
      source={{
        uri: 'http://facebook.github.io/react/img/logo_og.png',
        headers: {
          Authorization: 'someAuthToken'
        }
      }}
    />
    ```
    
    Note that the header values must be strings.

--- ReactAndroid/src/main/java/com/facebook/react/views/text/frescosupport/FrescoBasedReactTextInlineImageSpan.java
+++ ReactAndroid/src/main/java/com/facebook/react/views/text/frescosupport/FrescoBasedReactTextInlineImageSpan.java
@@ -25,7 +25,12 @@ import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.DraweeHolder;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableMapKeySetIterator;
 import com.facebook.react.views.text.TextInlineImageSpan;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * FrescoBasedTextInlineImageSpan is a span for Images that are inside <Text/>. It computes
@@ -48,14 +53,30 @@ public class FrescoBasedReactTextInlineImageSpan extends TextInlineImageSpan {
   private int mHeight;
   private Uri mUri;
   private int mWidth;
+  private ReadableMap mHeaders;
 
   private @Nullable TextView mTextView;
+
+  private static Map<String, String> readableMapToMap(ReadableMap readableMap) {
+    if (readableMap == null) {
+      return null;
+    }
+    ReadableMapKeySetIterator iterator = readableMap.keySetIterator();
+    Map<String, String> map = new HashMap<>();
+    while (iterator.hasNextKey()) {
+      String key = iterator.nextKey();
+      String value = readableMap.getString(key);
+      map.put(key, value);
+    }
+    return map;
+  }
 
   public FrescoBasedReactTextInlineImageSpan(
       Resources resources,
       int height,
       int width,
       @Nullable Uri uri,
+      ReadableMap headers,
       AbstractDraweeControllerBuilder draweeControllerBuilder,
       @Nullable Object callerContext) {
     mDraweeHolder = new DraweeHolder(
@@ -68,6 +89,7 @@ public class FrescoBasedReactTextInlineImageSpan extends TextInlineImageSpan {
     mHeight = height;
     mWidth = width;
     mUri = (uri != null) ? uri : Uri.EMPTY;
+    mHeaders = headers;
   }
 
   /**
@@ -127,6 +149,7 @@ public class FrescoBasedReactTextInlineImageSpan extends TextInlineImageSpan {
       Paint paint) {
     if (mDrawable == null) {
       ImageRequest imageRequest = ImageRequestBuilder.newBuilderWithSource(mUri)
+          .setHeaders(readableMapToMap(mHeaders))
           .build();
 
       DraweeController draweeController = mDraweeControllerBuilder
