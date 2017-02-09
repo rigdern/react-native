097d808097f80616df2c6481c5fdc299cb680656
Adam Comella <adcom@microsoft.com>
2017-02-08 23:36:15 -0800

    Don't set mFontScale in emitUpdateDimensionsEvent
    emitUpdateDimensionsEvent shouldn't have side effects. Instead, set it in onHostResume.

--- ReactAndroid/src/main/java/com/facebook/react/uimanager/UIManagerModule.java
+++ ReactAndroid/src/main/java/com/facebook/react/uimanager/UIManagerModule.java
@@ -139,7 +139,9 @@ public class UIManagerModule extends ReactContextBaseJavaModule implements
   public void onHostResume() {
     mUIImplementation.onHostResume();
 
-    if (mFontScale != getReactApplicationContext().getResources().getConfiguration().fontScale) {
+    float fontScale = getReactApplicationContext().getResources().getConfiguration().fontScale;
+    if (mFontScale != fontScale) {
+      mFontScale = fontScale;
       emitUpdateDimensionsEvent();
     }
   }
@@ -552,7 +554,6 @@ public class UIManagerModule extends ReactContextBaseJavaModule implements
   }
 
   public void emitUpdateDimensionsEvent() {
-    mFontScale = getReactApplicationContext().getResources().getConfiguration().fontScale;
     DisplayMetrics windowDisplayMetrics = DisplayMetricsHolder.getWindowDisplayMetrics();
     DisplayMetrics screenDisplayMetrics = DisplayMetricsHolder.getScreenDisplayMetrics();
 
